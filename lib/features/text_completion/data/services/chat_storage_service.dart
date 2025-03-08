5463351f9d0b0b416d22bf52233aa03561d8ec70
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-03-08 09:48:35 +0500

    Add shared_preferences and update Android build configuration
    - Added shared_preferences package to pubspec.lock
    - Updated Android Gradle configuration with Kotlin and build settings
    - Fixed import in chat_storage_service.dart
    - Updated text_completion_injection_container.dart to use shared_preferences

--- lib/features/text_completion/data/services/chat_storage_service.dart
+++ lib/features/text_completion/data/services/chat_storage_service.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../models/chat_message.dart';
 
 class ChatStorageService {
--- lib/features/text_completion/text_completion_injection_container.dart
+++ lib/features/text_completion/text_completion_injection_container.dart
@@ -1,17 +1,13 @@
-import 'package:flutter_chatgpt_text_and_image_processing/features/text_completion/data/remote_data_source/text_completion_remote_data_source.dart';
-import 'package:flutter_chatgpt_text_and_image_processing/features/text_completion/data/remote_data_source/text_completion_remote_data_source_impl.dart';
-import 'package:flutter_chatgpt_text_and_image_processing/features/text_completion/data/repositories/text_completion_repository_impl.dart';
-import 'package:flutter_chatgpt_text_and_image_processing/features/text_completion/domain/repositories/text_completion_repository.dart';
-import 'package:flutter_chatgpt_text_and_image_processing/features/text_completion/domain/usecases/text_completion_usecase.dart';
+import 'package:flutter_chatgpt_text_and_image_processing/features/text_completion/data/services/chat_storage_service.dart';
 import 'package:flutter_chatgpt_text_and_image_processing/features/text_completion/presentation/cubit/text_completion_cubit.dart';
 import 'package:flutter_chatgpt_text_and_image_processing/injection_container.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> textCompletionInjectionContainer() async {
   final prefs = await SharedPreferences.getInstance();
-  
+
   sl.registerLazySingleton(() => ChatStorageService(prefs));
-  
+
   sl.registerFactory<TextCompletionCubit>(
     () => TextCompletionCubit(sl()),
   );
